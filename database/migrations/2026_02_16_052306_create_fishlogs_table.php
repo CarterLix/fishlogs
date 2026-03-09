c0c073290d3f0bedd6a2345ba00a0c3243e70c13
Carter Lix <carterlix@gmail.com>
2026-03-09 13:04:59 -0600

    asd;lfjdf

--- database/migrations/2026_02_16_052306_create_fishlogs_table.php
+++ database/migrations/2026_02_16_052306_create_fishlogs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('fishlogs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->date('date');
             $table->string('name'); // name
             $table->string('location'); //location
